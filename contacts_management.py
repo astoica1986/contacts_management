edaf0ac34e328287119b19375c287fc28ffafec3
astoica1986 <stoica.adrian86@gmail.com>
2014-07-22 18:19:03 +0300

    a second try at frontend

--- contacts_management.py
+++ contacts_management.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask,jsonify
 from flask.ext.restless import APIManager
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, Text
-
+from sqlalchemy.orm import validates
 app = Flask(__name__, static_url_path='')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contact.db'
 db = SQLAlchemy(app)
@@ -10,16 +10,41 @@ db = SQLAlchemy(app)
 
 class Contacts(db.Model):
     id = Column(Integer, primary_key=True)
-    first_name = Column(Text, unique=False)
-    last_name = Column(Text, unique=False)
+    firstName = Column(Text(20), unique=False)
+    lastName = Column(Text(20), unique=False)
     phone = Column(Integer, unique=True)
     email = Column(Text, unique=True)
+
+    @validates('email')
+    def validate_email(self, key, address):
+        try:
+            assert '@' in address
+        except AssertionError:
+            return jsonify(error="Invalid JSON Provided")
+        return address
 
 
 db.create_all()
 
+
 api_manager = APIManager(app, flask_sqlalchemy_db=db)
-api_manager.create_api(Contacts, methods=['GET', 'POST', 'DELETE', 'PUT'])
+
+
+def get_many_postprocessor(result=None, **kw):
+    #result = dict((key, value) for key, value in result.items() if key == 'objects')
+    #print(result)
+     # Grab the list of objects so we can wipe the result dict
+    temp = result['objects']
+    # Remove all key/value pairs from the result dict
+    result.clear()
+    # Add the 'persons' key and set it equal to the list
+    # of objects from the original result dict
+    result['objects'] = temp
+
+
+api_manager.create_api(Contacts,
+                       methods=['GET', 'POST', 'DELETE', 'PUT'],
+                       postprocessors={'GET_MANY': [get_many_postprocessor]})
 
 
 @app.route('/')
--- static/js/app.js
+++ static/js/app.js
@@ -1,69 +1,11 @@
-angular.module('contact', ['restangular', 'ngRoute']).
-  config(function($routeProvider, RestangularProvider) {
-    $routeProvider.
-      when('/', {
-        controller:ListCtrl,
-        templateUrl:'list.html'
-      }).
-      when('/edit/:contactId', {
-        controller:EditCtrl,
-        templateUrl:'detail.html',
-        resolve: {
-          contact: function(Restangular, $route){
-            return Restangular.one('contacts', $route.current.params.contactId).get();
-          }
-        }
-      }).
-      when('/new', {controller:CreateCtrl, templateUrl:'detail.html'}).
-      otherwise({redirectTo:'/'});
+'use strict';
 
-      RestangularProvider.setBaseUrl('http://localhost:5000/api');
-      RestangularProvider.setRestangularFields({
-        id: '_id.$oid'
-      });
+// Declare app level module which depends on filters, and services
+angular.module('ngdemo', ['ngdemo.filters', 'ngdemo.services', 'ngdemo.directives', 'ngdemo.controllers']).
+    config(['$routeProvider', function ($routeProvider) {
 
-      RestangularProvider.setRequestInterceptor(function(elem, operation, what) {
-
-        if (operation === 'put') {
-          elem._id = undefined;
-          return elem;
-        }
-        return elem;
-      })
-  });
-
-
-function ListCtrl($scope, Restangular) {
-   $scope.contacts = Restangular.all("contacts").getList().$object;
-}
-
-
-function CreateCtrl($scope, $location, Restangular) {
-  $scope.save = function() {
-    Restangular.all('contacts').post($scope.contact).then(function(contact) {
-      $location.path('/list');
-    });
-  }
-}
-
-function EditCtrl($scope, $location, Restangular, contact) {
-  var original = contact;
-  $scope.contact = Restangular.copy(original);
-
-
-  $scope.isClean = function() {
-    return angular.equals(original, $scope.contact);
-  }
-
-  $scope.destroy = function() {
-    original.remove().then(function() {
-      $location.path('/list');
-    });
-  };
-
-  $scope.save = function() {
-    $scope.contact.put().then(function() {
-      $location.path('/');
-    });
-  };
-}
+        $routeProvider.when('/', {templateUrl: 'partials/user-list.html', controller: 'UserListCtrl'});
+        $routeProvider.when('/user-detail/:id', {templateUrl: 'partials/user-detail.html', controller: 'UserDetailCtrl'});
+        $routeProvider.when('/user-creation', {templateUrl: 'partials/user-creation.html', controller: 'UserCreationCtrl'});
+        $routeProvider.otherwise({redirectTo: ''});
+    }]);
